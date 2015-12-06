bd7c4f8eac26c6162ec51f77329232e4491375f7
Matt Weagle <mweagle@gmail.com>
2015-12-06 09:23:44 -0800

    Initial commit of IP->Geo information
    Inspired by: https://github.com/tmaiaroto/go-lambda-geoip
    
    Sample response:
    
    ```
    {
      "code": 200,
      "status": "OK",
      "headers": {
        "content-type": "application/json",
        "date": "Sun, 06 Dec 2015 17:21:22 GMT",
        "content-length": "984"
      },
      "results": {
        "Info": {
          "City": {
            "GeoNameID": 0,
            "Names": null
          },
          "Continent": {
            "Code": "NA",
            "GeoNameID": 6255149,
            "Names": {
              "de": "Nordamerika",
              "en": "North America",
              "es": "Norteamérica",
              "fr": "Amérique du Nord",
              "ja": "北アメリカ",
              "pt-BR": "América do Norte",
              "ru": "Северная Америка",
              "zh-CN": "北美洲"
            }
          },
          "Country": {
            "GeoNameID": 6252001,
            "IsoCode": "US",
            "Names": {
              "de": "USA",
              "en": "United States",
              "es": "Estados Unidos",
              "fr": "États-Unis",
              "ja": "アメリカ合衆国",
              "pt-BR": "Estados Unidos",
              "ru": "США",
              "zh-CN": "美国"
            }
          },
          "Location": {
            "Latitude": 0,
            "Longitude": 0,
            "MetroCode": 0,
            "TimeZone": ""
          },
          "Postal": {
            "Code": ""
          },
          "RegisteredCountry": {
            "GeoNameID": 6252001,
            "IsoCode": "US",
            "Names": {
              "de": "USA",
              "en": "United States",
              "es": "Estados Unidos",
              "fr": "États-Unis",
              "ja": "アメリカ合衆国",
              "pt-BR": "Estados Unidos",
              "ru": "США",
              "zh-CN": "美国"
            }
          },
          "RepresentedCountry": {
            "GeoNameID": 0,
            "IsoCode": "",
            "Names": null,
            "Type": ""
          },
          "Subdivisions": null,
          "Traits": {
            "IsAnonymousProxy": false,
            "IsSatelliteProvider": false
          }
        }
      }
    }
    ```

---
+++ Makefile
@@ -0,0 +1,50 @@
+.DEFAULT_GOAL=provision
+.PHONY: build test get run tags
+
+ensure_vendor:
+	mkdir -pv vendor
+
+clean:
+	rm -rf ./vendor
+	go clean .
+
+format:
+	go fmt .
+	gofmt -s -w ./transforms/
+
+vet:
+	go vet .
+
+generate:
+	go generate -x
+
+get: clean ensure_vendor
+	git clone --depth=1 https://github.com/aws/aws-sdk-go ./vendor/github.com/aws/aws-sdk-go
+	rm -rf ./src/main/vendor/github.com/aws/aws-sdk-go/.git
+	git clone --depth=1 https://github.com/vaughan0/go-ini ./vendor/github.com/vaughan0/go-ini
+	rm -rf ./src/main/vendor/github.com/vaughan0/go-ini/.git
+	git clone --depth=1 https://github.com/Sirupsen/logrus ./vendor/github.com/Sirupsen/logrus
+	rm -rf ./src/main/vendor/github.com/Sirupsen/logrus/.git
+	git clone --depth=1 https://github.com/voxelbrain/goptions ./vendor/github.com/voxelbrain/goptions
+	rm -rf ./src/main/vendor/github.com/voxelbrain/goptions/.git
+	git clone --depth=1 https://github.com/mjibson/esc ./vendor/github.com/mjibson/esc
+	rm -rf ./src/main/vendor/github.com/mjibson/esc/.git
+	git clone --depth=1 https://github.com/mweagle/Sparta ./vendor/github.com/mweagle/Sparta
+	rm -rf ./src/main/vendor/github.com/mweagle/Sparta/.git
+	git clone --depth=1 https://github.com/oschwald/geoip2-golang ./vendor/github.com/oschwald/geoip2-golang
+	rm -rf ./src/main/vendor/github.com/oschwald/geoip2-golang/.git
+
+build: get format vet generate
+	GO15VENDOREXPERIMENT=1 go build .
+
+test: build
+	GO15VENDOREXPERIMENT=1 go test ./test/...
+
+tags:
+	gotags -tag-relative=true -R=true -sort=true -f="tags" -fields=+l .
+
+provision:
+	go run main.go --level debug provision --s3Bucket $(S3_BUCKET)
+
+describe:
+	go run main.go --level info describe --out ./graph.html
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+
+	"SpartaGeoIP/constants"
+
+	"github.com/Sirupsen/logrus"
+	sparta "github.com/mweagle/Sparta"
+	"github.com/oschwald/geoip2-golang"
+)
+
+//go:generate mkdir -pv ./constants
+//go:generate esc -o ./constants/CONSTANTS.go -pkg constants GeoLite2-Country.mmdb
+
+////////////////////////////////////////////////////////////////////////////////
+// IP->Geo results
+//
+func ipGeoLambda(event *json.RawMessage, context *sparta.LambdaContext, w http.ResponseWriter, logger *logrus.Logger) {
+	var lambdaEvent sparta.APIGatewayLambdaJSONEvent
+	err := json.Unmarshal([]byte(*event), &lambdaEvent)
+	if err != nil {
+		logger.Error("Failed to unmarshal event data: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	db, err := geoip2.FromBytes(constants.FSMustByte(false, "/GeoLite2-Country.mmdb"))
+	if err != nil {
+		logger.Error("Failed to open Database: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	parsedIp := net.ParseIP(lambdaEvent.Context.Identity.SourceIP)
+	record, err := db.City(parsedIp)
+	if err != nil {
+		logger.Error("Failed to find city: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	httpResponse := map[string]interface{}{
+		"Info": record,
+	}
+
+	responseBody, err := json.Marshal(httpResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, string(responseBody))
+	}
+}
+
+func main() {
+	stage := sparta.NewStage("ipgeo")
+	apiGateway := sparta.NewAPIGateway("SpartaGeoIPService", stage)
+	stackName := "SpartaGeoIP"
+
+	var lambdaFunctions []*sparta.LambdaAWSInfo
+	lambdaFn := sparta.NewLambda(sparta.IAMRoleDefinition{}, ipGeoLambda, nil)
+	apiGatewayResource, _ := apiGateway.NewResource("/ipgeo", lambdaFn)
+	apiGatewayResource.NewMethod("GET")
+	lambdaFunctions = append(lambdaFunctions, lambdaFn)
+
+	sparta.Main(stackName,
+		"Sparta app supporting ip->geo mapping",
+		lambdaFunctions,
+		apiGateway)
+}
